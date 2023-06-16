4288171789e3299fffa700d2014686423c8cc339
Aditya Nath <aditya.k.nath@gmail.com>
2023-06-17 04:50:27 +0930

    Convert project to use typescript

---
+++ dist/templates/01_blank_template/app.js
@@ -0,0 +1,11 @@
+/* 
+    TITLE: Webkit-CLI (webkit-cli) - Blank
+    TEMPLATE INFO: Blank Template - Empty html,css,js files
+    AUTHOR: AdityaNathK
+    ORGANISATION: KronosApps
+    CONTACT: apps.kronos@gmail.com
+    LICENSE: MIT 
+*/
+
+/* JavaScript simple alert function */
+alert("Hello, World!");
